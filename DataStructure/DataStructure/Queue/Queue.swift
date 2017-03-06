29a483967a714df83569a22d73b5b73f2de84302
tarun-xoriant <tarun.sharma@xoriant.com>
2017-03-06 20:49:36 +0530

    Added queue interface and queue ds file

---
+++ DataStructure/DataStructure/Queue/Queue.swift
@@ -0,0 +1,9 @@
+//
+//  Queue.swift
+//  DataStructure
+//
+//  Created by Tarun Sharma on 3/6/17.
+//  Copyright © 2017 Tarun Sharma. All rights reserved.
+//
+
+import Foundation
---
+++ DataStructure/DataStructure/Queue/QueueProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  QueueProtocol.swift
+//  DataStructure
+//
+//  Created by Tarun Sharma on 3/6/17.
+//  Copyright © 2017 Tarun Sharma. All rights reserved.
+//
+
+import Foundation
