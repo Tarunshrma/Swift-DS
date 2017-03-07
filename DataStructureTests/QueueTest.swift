e6e5cab236d6daf933beb46194758f97d99cd887
tarun-xoriant <tarun.sharma@xoriant.com>
2017-03-07 11:52:47 +0530

    Added test cases for enqueue and dequeue methods

--- DataStructureTests/QueueTest.swift
+++ DataStructureTests/QueueTest.swift
@@ -23,6 +23,7 @@ class QueueTest: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        queue = nil
     }
 
     //test case to check if method is empty
@@ -56,5 +57,93 @@ class QueueTest: XCTestCase {
         //Then
         XCTAssertFalse(empty, "Queue should not be empty!")
     }
+    
+    func testEnqeue()
+    {
+        //Given
+        let expectedFrontItem = "A"
+        do{
+            try queue?.enqueue(item: expectedFrontItem)
+            try queue?.enqueue(item: "B")
+            try queue?.enqueue(item: "C")
+            try queue?.enqueue(item: "D")
+        }catch
+        {
+            XCTFail("Exception raised in insertion of queue")
+        }
+        
+        var actualFrontItem:String = ""
+        //When
+        do{
+            actualFrontItem  = try queue!.dequeue()
+        }catch
+        {
+            XCTFail("Exception raised in dequeue")
+        }
+        //Then
+        XCTAssert(expectedFrontItem == actualFrontItem, "item \(expectedFrontItem) should be front item but currently it is set to \(actualFrontItem)")
+    }
+    
+    func testEnqeueWhenQueueIsFull()
+    {
+        //Given
+        do{
+            try queue?.enqueue(item: "A")
+            try queue?.enqueue(item: "B")
+            try queue?.enqueue(item: "C")
+            try queue?.enqueue(item: "D")
+            try queue?.enqueue(item: "E")
+            try queue?.enqueue(item: "F") //This should not be allowed
+        }catch QueueException.queueFull
+        {
+            XCTAssert(true, "Queue should be full!")
+        }catch
+        {
+            XCTFail("Exception raised in enqueue")
+        }
+    }
+    
+    func testDeQeueWhenQueueIsEmpty()
+    {
+        //Given
+        do{
+           _ = try queue?.dequeue()
+        }catch QueueException.queueEmpty
+        {
+            XCTAssert(true, "Queue should be empty!")
+        }catch
+        {
+            XCTFail("Exception raised in enqueue")
+        }
+    }
+    
+    func testdeQueue()
+    {
+        //Given
+        let expectedDequeueItem = "C"
+        var actualDequeueItem = ""
+        
+        do{
+            try queue?.enqueue(item: "A")
+            try queue?.enqueue(item: "B")
+            try queue?.enqueue(item: "C")
+            try queue?.enqueue(item: "D")
+            try queue?.enqueue(item: "E")
+            
+            _ = try queue?.dequeue()
+            _ = try queue?.dequeue()
+            actualDequeueItem = try (queue?.dequeue())!
+            
+        }catch
+        {
+            XCTFail("Exception raised in enqueue/dequeue of queue")
+        }
+        
+       
+        //Then
+        XCTAssert(expectedDequeueItem == actualDequeueItem, "item \(expectedDequeueItem) should be front item but currently it is set to \(actualDequeueItem)")
+    }
+
+
     
 }
